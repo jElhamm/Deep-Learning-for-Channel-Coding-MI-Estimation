b573d113fe1f9b845e62de67f5e7dde8e2612651
jelhamm <elhamjafari200312@gmail.com>
2024-07-26 16:45:25 +0430

    Implement Initial Setup for AutoEncoder Clas

--- Source Code/Python Sources File/AutoEncoder_Rayleigh_Channel_MINE.py
+++ Source Code/Python Sources File/AutoEncoder_Rayleigh_Channel_MINE.py
@@ -30,3 +30,31 @@ assert sys.version_info >= (3, 5)
 np.random.seed(42)
 tf.random.set_seed(42)
    
+
+# ------------------------------------------------ The AutoEncoder class models a 16-QAM communication system using neural networks ------------------------------------------------
+
+class AutoEncoder:
+    def __init__(self, M=16, n=1, training_snr=7, rayleigh=False):
+        self.M = M
+        self.k = int(np.log2(M))
+        self.n = n
+        self.training_snr = training_snr
+        self.rayleigh = rayleigh
+        self.noise_std = self.EbNo_to_noise(training_snr)
+
+        # ---------------------------------------------------- Define custom layers -----------------------------------------------------------
+
+        self.norm_layer = keras.layers.Lambda(lambda x: tf.divide(x, tf.sqrt(2 * tf.reduce_mean(tf.square(x)))))
+        self.shape_layer = keras.layers.Lambda(lambda x: tf.reshape(x, shape=[-1, 2, n]))
+        self.shape_layer2 = keras.layers.Lambda(lambda x: tf.reshape(x, shape=[-1, 2 * n]))
+        self.channel_layer = keras.layers.Lambda(lambda x: x + tf.random.normal(tf.shape(x), mean=0.0, stddev=self.noise_std))
+
+        # --------------------------------------------------- Define the encoder model --------------------------------------------------------
+        self.encoder = keras.models.Sequential([
+            keras.layers.Embedding(M, M, embeddings_initializer='glorot_normal', input_length=1),
+            keras.layers.Dense(M, activation="elu"),
+            keras.layers.Dense(2 * n, activation=None),
+            self.shape_layer,
+            self.norm_layer
+        ])
+    
